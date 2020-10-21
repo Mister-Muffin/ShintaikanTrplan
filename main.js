0c0de32c2069f80600db6446c08f44b38337b215
Mister-Muffin <markus.aa84@gmail.com>
2020-10-21 23:42:34 +0200

    yaman

--- main.js
+++ main.js
@@ -10,43 +10,17 @@
 //              y: Einheit
 
 const db = firebase.firestore();
-
 const documentFix = "wUWqwE3QsztMFEfVlc9U";
 
-const protectedUrl = "https://europe-west1-shintaikan-6b670.cloudfunctions.net/getKey";
+const maxParticipiant = 10;
 
-async function get(collection, key) {
-    const response = await fetch(protectedUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            "collection": collection,
-            "document": key
-        })
+function getKey(collection, index) {
+    db.collection(collection).get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            return (doc.data().maxLessons[index])
+        });
     });
-    const status = response.status;
-    if (status === 200) {
-        return response.json();
-    } else {
-        return status;
-    }
-}
-
-
-let res = get("fix", documentFix);
-res.then(function (result) {
-    console.log(result)
-})
-
-
-get("fix", documentFix).then(function (result) {
-    console.log(result)
-});
-
-
-
+};
 
 /**
  * Funktionen für die Erste Funktion:
@@ -56,13 +30,48 @@ get("fix", documentFix).then(function (result) {
  */
 
 
+function updateData(collection, doc, obj) {
+    const ref = db.collection(collection).doc(doc);
+    ref.update({
+        training_counter: obj
+    }).then(function () {
+        console.log("Document successfully updated!");
+    }).catch(function (error) {
+        console.error("Error updating document: ", error);
+    });
+}
+
+
+
 function initCounter() {
-    for (var i = 0; i < 5; i++) {
-        for (var j = 0; j < 5; j++) {
-            alert("Test");
-        };
-    };
+    db.collection("fix").get().then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+            var mapOutside = {};
+            for (var i = 1; i <= 5; i++) {
+                for (var j = 1; j <= doc.data().maxLessons[i - 1]; j++) {
+                    // console.log(i.toString() + j.toString());
+                    var mapInside = {};
+
+                    mapInside.trainer = 1;
+                    mapInside.current = maxParticipiant - 1;
+                    mapOutside[i + '' + j] = mapInside;
+
+
+                };
+            };
+
+            // doc.data().trainingCounter(i.toString() + j.toString()).trainer = 1;
+            // const tmp = doc.data().trainingCounter(i.toString() + j.toString()).trainer
+            // doc.data().trainingCounter(i.toString() + j.toString()).current = maxParticipiant - tmp;
+
+            updateData("fix", documentFix, mapOutside);
+            console.log(mapOutside);
+        });
+    });
 };
+
+initCounter();
+
 
 /**
  * Hint:
@@ -70,7 +79,7 @@ function initCounter() {
  * the first leson on Tuesday: day = 1, lesson = 0
  */
 function setTrainerNumber(day, lesson, trainer) {
-
+    
 };
 
 
