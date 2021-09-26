abf002ac33e98320407d76ee64b534206816e42a
Mister-Muffin <45641704+Mister-Muffin@users.noreply.github.com>
2021-09-26 20:26:29 +0200

    added vue project

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <table style="width: 100%">
+    <tbody v-if="response">
+      <tr>
+        <td>
+          <p><em> Montag </em></p>
+        </td>
+        <td>
+          <p><em> Dienstag </em></p>
+        </td>
+        <td>
+          <p><em> Mittwoch </em></p>
+        </td>
+        <td>
+          <p><em> Donnerstag </em></p>
+        </td>
+        <td>
+          <p><em> Freitag </em></p>
+        </td>
+      </tr>
+      <tr v-for="i in 5" :key="i">
+        <td v-for="j in 5" :key="j">
+          <HelloWorld
+            v-if="response[`${i}` + `${j}`]"
+            :db="db"
+            :day="j"
+            :session="i"
+            :data="response[`${i}` + `${j}`]"
+          />
+          <HelloWorld v-else :db="db" :day="j" :session="i" :data="{}" />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAd5SQiFe5RRm954rTLRnEPPGEYznSMSBo",
+  authDomain: "shintaikan-6b670.firebaseapp.com",
+  databaseURL: "https://shintaikan-6b670.firebaseio.com",
+  projectId: "shintaikan-6b670",
+  storageBucket: "shintaikan-6b670.appspot.com",
+  messagingSenderId: "929946590509",
+  appId: "1:929946590509:web:7107bf53389ab56dc507a4",
+};
+
+// Initialize Firebase
+// eslint-disable-next-line no-unused-vars
+initializeApp(firebaseConfig);
+let i = 1;
+let j = 1;
+const db = getFirestore();
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      docdata: null,
+      response: {},
+      user: "",
+    };
+  },
+  async mounted() {
+    
+    const querySnapshot = await getDocs(collection(db, "times"));
+    querySnapshot.forEach((doc) => {
+      this.response[`${doc.id}`] = doc.data();
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+    console.log(JSON.parse(JSON.stringify(this.$data.response)));
+    console.log(
+      JSON.parse(JSON.stringify(this.$data.response[`${i}` + `${j}`]))
+    );
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+td {
+  border: 1px solid silver;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,76 @@
+<template>
+  <div v-if="time" v-bind:style="{ color: color }" class="hello">
+    <span>{{ time }} - {{ end }}</span>
+    <span>{{ selected }}</span>
+  </div>
+</template>
+
+<script>
+import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "@firebase/app";
+
+initializeApp({
+  apiKey: "AIzaSyAd5SQiFe5RRm954rTLRnEPPGEYznSMSBo",
+  authDomain: "shintaikan-6b670.firebaseapp.com",
+  databaseURL: "https://shintaikan-6b670.firebaseio.com",
+  projectId: "shintaikan-6b670",
+  storageBucket: "shintaikan-6b670.appspot.com",
+  messagingSenderId: "929946590509",
+  appId: "1:929946590509:web:7107bf53389ab56dc507a4",
+});
+
+getFirestore();
+
+export default {
+  name: "HelloWorld",
+  props: {
+    day: Number,
+    session: Number,
+    data: Object,
+  },
+  data() {
+    return {
+      selected: this.data.group,
+      time: this.data.start,
+      end: this.data.end,
+      customText: this.data.customText,
+      color: this.data.color,
+    };
+  },
+};
+//console.log(this.data);
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  margin-bottom: 3vh;
+}
+.hello {
+  display: grid;
+}
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: bold;
+  font-size: 30px;
+}
+
+body {
+  text-align: center;
+}
+
+table {
+  width: 100%;
+  height: 100%;
+}
+
+.header {
+  color: #ffffff;
+}
+
+td {
+  border: 2px solid silver;
+  width: 20%;
+  height: 16, 6%;
+}
+</style>
